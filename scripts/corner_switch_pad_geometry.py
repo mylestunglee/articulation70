eeea3e88ef305a064bcfa5715b1cad28f6490f1b
Myles Lee <mylestunglee@gmail.com>
2023-10-30 00:40:00 +0000

    use edge cuts for overlapping drill holes
    adjust pads to 0.3mm clearance

---
+++ scripts/corner_switch_pad_geometry.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+a = np.array([-3.1, -6.4])
+x1 = np.array([-4.875, -7.25])
+x2 = np.array([-2.275, -7.25])
+x3 = np.array([-2.275, -4.65])
+c1 = np.array([-5, -3.75])
+clearance = 0.3
+radius = 1.5 + clearance
+segments = 64
+
+theta1 = np.arcsin((c1[1] - x3[1]) / radius)
+theta2 = np.arccos((x1[0] - c1[0]) / radius)
+pad_radius = 1.0 / np.cos((theta2 - theta1) / (2 * segments))
+
+def print_point(x):
+    print('      (xy {} {})'.format(round(x[0], 4), round(x[1], 4)))
+
+print_point(x1 - a)
+print_point(x2 - a)
+print_point(x3 - a)
+for theta in np.linspace(theta1, theta2, 64):
+    print_point(c1 + radius * pad_radius * np.array([np.cos(theta), -np.sin(theta)]) - a)
--- scripts/curved_switch_pad_geometry.py
+++ scripts/curved_switch_pad_geometry.py
@@ -1,14 +1,17 @@
 import numpy as np
 
+clearance = 0.3
+radius = 1.5 + clearance
 a = np.array([-3.1, -7.4])
 x1 = np.array([-4.875, -8.25])
 x2 = np.array([-2.275, -8.25])
 x3 = np.array([-2.275, -3.65])
-x4 = np.array([-3.25, -3.65])
+x4 = np.array([-5 + radius, -3.65])
 c1 = np.array([-5, -4.75])
-radius = 1.5 + 0.25
+segments = 64
 
 theta = np.arccos((x1[0] - c1[0]) / radius)
+pad_radius = 1.0 / np.cos(theta / (2 * segments))
 
 def print_point(x):
     print('      (xy {} {})'.format(round(x[0], 4), round(x[1], 4)))
@@ -17,5 +20,5 @@ print_point(x1 - a)
 print_point(x2 - a)
 print_point(x3 - a)
 print_point(x4 - a)
-for theta in np.linspace(0, theta, 16):
-    print_point(c1 + radius * np.array([np.cos(theta), -np.sin(theta)]) - a)
+for theta in np.linspace(0, theta, 64):
+    print_point(c1 + radius * pad_radius * np.array([np.cos(theta), -np.sin(theta)]) - a)
