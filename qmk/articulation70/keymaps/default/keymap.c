decb4055e304e276bd0fc1303fbb60c1e8249e8e
Myles Lee <mylestunglee@gmail.com>
2024-06-15 14:19:44 +0100

    Update personal keymap

--- qmk/articulation70/keymaps/default/keymap.c
+++ qmk/articulation70/keymaps/default/keymap.c
@@ -21,10 +21,10 @@ enum Layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_BASE] = LAYOUT_split(
-        KC_ESC  , KC_F4   , KC_W    , KC_F    , KC_P    , KC_B              ,           KC_J    , KC_L    , KC_U    , KC_Y    , KC_PSCR , KC_BSPC , KC_INS  , KC_HOME , KC_PGUP ,
-        KC_TAB  , KC_Q    , KC_R    , KC_S    , KC_T    , KC_G              ,           KC_M    , KC_N    , KC_E    , KC_I    , KC_SCLN , KC_ENT  , KC_DEL  , KC_END  , KC_PGDN ,
-        KC_LSFT , KC_A    , KC_X    , KC_C    , KC_D    , KC_V              ,           KC_K    , KC_H    , KC_COMM , KC_DOT  , KC_O    , KC_RSFT           , KC_UP   ,
-        KC_LCTL , KC_Z    ,                     TL_LOWR , KC_BSPC , KC_LALT , KC_RGUI , KC_SPC  , TL_UPPR                     , KC_SLSH , KC_RCTL , KC_LEFT , KC_DOWN , KC_RGHT
+        KC_ESC  , KC_F4   , KC_L    , KC_D    , KC_W    , KC_Q              ,           KC_QUOT , KC_F    , KC_O    , KC_U    , KC_PSCR , KC_BSPC , KC_INS  , KC_HOME , KC_PGUP ,
+        KC_TAB  , KC_B    , KC_R    , KC_T    , KC_S    , KC_G              ,           KC_Y    , KC_H    , KC_A    , KC_E    , KC_J    , KC_ENT  , KC_DEL  , KC_END  , KC_PGDN ,
+        KC_LSFT , KC_N    , KC_X    , KC_M    , KC_C    , KC_V              ,           KC_K    , KC_P    , KC_COMM , KC_DOT  , KC_I    , KC_RSFT           , KC_UP   ,
+        KC_LCTL , KC_Z    ,                     KC_LALT , KC_BSPC , TL_LOWR , TL_UPPR , KC_SPC  , KC_RGUI                     , KC_SLSH , KC_RCTL , KC_LEFT , KC_DOWN , KC_RGHT
     ),
     [LAYER_NUMPAD] = LAYOUT_split(
         _______ , _______ , _______ , _______ , _______ , _______           ,           KC_PSLS , KC_P7   , KC_P8   , KC_P9   , KC_PMNS , _______ , _______ , _______ , _______ ,
@@ -39,30 +39,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______ , _______ ,                     _______ , _______ , _______ , _______ , _______ , _______                     , KC_LCTL , UK_BSLS , _______ , _______ , _______
     ),
     [LAYER_LOWER] = LAYOUT_split(
-        KC_VOLD , KC_VOLU , UK_HASH , KC_QUOT , KC_SCLN , KC_NO             ,           KC_NO   , KC_AMPR , KC_ASTR , KC_LPRN , KC_NO   , _______ , _______ , _______ , _______ ,
-        _______ , KC_GRV  , KC_LPRN , KC_RPRN , KC_MINS , KC_NO             ,           KC_NO   , KC_DLR  , KC_PERC , KC_CIRC , KC_RPRN , _______ , _______ , _______ , _______ ,
-        _______ , KC_EQL  , KC_LCBR , KC_RCBR , KC_NO   , KC_NO             ,           KC_NO   , KC_EXLM , UK_DQUO , UK_PND  , KC_NO   , _______           , _______ ,
+        KC_VOLD , KC_VOLU , UK_HASH , KC_QUOT , KC_SCLN , KC_MOST           ,           KC_NUM  , KC_AMPR , KC_ASTR , KC_LPRN , KC_NO   , _______ , _______ , _______ , _______ ,
+        _______ , KC_GRV  , KC_LPRN , KC_RPRN , KC_MINS , KC_MOST           ,           KC_CAPS , KC_DLR  , KC_PERC , KC_CIRC , KC_RPRN , _______ , _______ , _______ , _______ ,
+        _______ , KC_EQL  , KC_LCBR , KC_RCBR , KC_NO   , KC_MOST           ,           KC_SCRL , KC_EXLM , UK_DQUO , UK_PND  , KC_NO   , _______           , _______ ,
         _______ , UK_BSLS ,                     _______ , _______ , _______ , _______ , _______ , _______                     , KC_NO   , _______ , _______ , _______ , _______
     ),
     [LAYER_UPPER] = LAYOUT_split(
-        KC_VOLD , KC_VOLU , UK_TILD , UK_AT   , KC_COLN , KC_NO             ,           KC_NUM  , KC_7    , KC_8    , KC_9    , KC_MINS , KC_TGNU , _______ , _______ , _______ ,
-        _______ , UK_NOT  , KC_LBRC , KC_RBRC , KC_UNDS , KC_NO             ,           KC_CAPS , KC_4    , KC_5    , KC_6    , KC_PLUS , KC_PENT , _______ , _______ , _______ ,
-        _______ , KC_PLUS , KC_LCBR , KC_RCBR , KC_NO   , KC_NO             ,           KC_SCRL , KC_1    , KC_2    , KC_3    , KC_0    , _______           , _______ ,
+        KC_VOLD , KC_VOLU , UK_TILD , UK_AT   , KC_COLN , KC_MOST           ,           KC_NUM  , KC_7    , KC_8    , KC_9    , KC_MINS , KC_TGNU , _______ , _______ , _______ ,
+        _______ , UK_NOT  , KC_LBRC , KC_RBRC , KC_UNDS , KC_MOST           ,           KC_CAPS , KC_4    , KC_5    , KC_6    , KC_PLUS , KC_PENT , _______ , _______ , _______ ,
+        _______ , KC_PLUS , KC_LCBR , KC_RCBR , KC_NO   , KC_MOST           ,           KC_SCRL , KC_1    , KC_2    , KC_3    , KC_0    , _______           , _______ ,
         _______ , UK_PIPE ,                     _______ , _______ , _______ , _______ , _______ , _______                     , KC_DOT  , _______ , _______ , _______ , _______
     ),
     [LAYER_ADJUST] = LAYOUT_split(
-        KC_TGGA , KC_MOST , KC_NO   , KC_NO   , KC_NO   , KC_NO             ,           KC_NO   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ , _______ ,
-        _______ , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO             ,           KC_NO   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , _______ , _______ , _______ , _______ ,
-        _______ , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO             ,           KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______           , _______ ,
+        KC_TGGA , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_MOST           ,           KC_NUM  , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ , _______ ,
+        _______ , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_MOST           ,           KC_CAPS , KC_F5   , KC_F6   , KC_F7   , KC_F8   , _______ , _______ , _______ , _______ ,
+        _______ , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_MOST           ,           KC_SCRL , KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______           , _______ ,
         _______ , KC_NO   ,                     _______ , _______ , _______ , _______ , _______ , _______                     , KC_NO   , _______ , _______ , _______ , _______
     ),
     [LAYER_STATUS] = LAYOUT_split(
-        KC_TGGA , KC_MOST , _______ , _______ , _______ , _______           ,           _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-        _______ , _______ , _______ , _______ , _______ , _______           ,           _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-        _______ , _______ , _______ , _______ , _______ , _______           ,           _______ , _______ , _______ , _______ , _______ , _______           , _______ ,
+        KC_TGGA , _______ , _______ , _______ , _______ , KC_MOST           ,           _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , KC_MOST           ,           _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , KC_MOST           ,           _______ , _______ , _______ , _______ , _______ , _______           , _______ ,
         _______ , _______ ,                     _______ , _______ , _______ , _______ , _______ , _______                     , _______ , _______ , _______ , _______ , _______
     )
-}; 
+};
 
 bool g_blink_leds;
 led_t g_led_state;
